d581a5b901b96e18dfa4c9015b168f7a99eaea95
czara804 <cbzara@gmail.com>
2020-03-10 20:23:10 +1100

    css styles

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,7 @@ class Product < ApplicationRecord
 
   def self.search(search)
     if search
-      where(["name LIKE ?","%#{search}%"])
+      where(["name ILIKE ?","%#{search}%"])
     else
       all
     end
